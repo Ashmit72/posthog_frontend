47ec1ac7317505d3cc1eced84e7b1c3fccc9eb3d
ashmit <ashmit@ashmit.ashmit>
2024-09-30 06:24:21 +0545

    uploads13

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // Proxy requests starting with /api to your backend server
       '/': {
-        target: 'http://13.233.168.114',
+        target: 'https://13.233.168.114:8080',
         changeOrigin: true, // Needed for virtual hosted sites
         secure: false, // Set to false if you are using a self-signed certificate
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove /api prefix
